1f4ac29b1b3da550cbbb2d9c96e8677acd5df6b0
AlaxDav1d <alaxdavid13@gmail.com>
2023-03-20 14:16:31 -0300

    Testando menu Nav

---
+++ Outono.js
@@ -0,0 +1,8 @@
+var sla = document.querySelector("nav");
+function aparece(){
+    sla.style.opacity = 1;
+}
+function some(){
+    sla.style.opacity = 0 ;
+    sla.style.transitionDuration = "800ms";
+}
